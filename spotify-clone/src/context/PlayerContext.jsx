60474827784d864aaf372683a9322ba6192163f7
Sanjay Kumar Sharma <125959746+sanjaysharma11@users.noreply.github.com>
2025-06-18 03:58:14 +0530

    Update PlayerContext.jsx

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -9,7 +9,7 @@ const PlayerContextProvider = (props) => {
     const audioRef = useRef();
     const seekBg = useRef();
     const seekBar = useRef();
-    const url = 'http://localhost:4000'
+    const url = 'https://spotify-full-stack-g2gg.onrender.com'
 
     const [songsData, setSongsData] = useState([]);
     const [albumsData,setAlbumData] = useState([]);
